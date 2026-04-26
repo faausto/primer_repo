4492c84d7875b698559e190aaa5dc16019a86823
Fausto Ottino <ottinofausto@gmail.com>
2026-04-26 18:56:00 -0300

    Agregar sección de hobbies al script

--- mi_info.py
+++ mi_info.py
@@ -2,6 +2,12 @@
 nombre = "Fausto Ottino"
 edad = 31 
 materia = "Software Factory"
+
+
 print(f"Hola, soy {nombre}")
 print(f"Tengo {edad} años")
 print(f"Estoy cursando {materia}")
+
+# Mis hobbies
+hobbies = ["programar", "jugar videojuegos", "escuchar música"]
+print(f"Mis hobbies son: {', '.join(hobbies)}")
