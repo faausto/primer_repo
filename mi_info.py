bd4814c015a11eb1b7e3481a243aa6dc1da03031
Fausto Ottino <ottinofausto@gmail.com>
2026-04-26 18:51:33 -0300

    Agregar script con información personal

---
+++ mi_info.py
@@ -0,0 +1,7 @@
+# mi_info.py - Información personal
+nombre = "Fausto Ottino"
+edad = 31 
+materia = "Software Factory"
+print(f"Hola, soy {nombre}")
+print(f"Tengo {edad} años")
+print(f"Estoy cursando {materia}")
